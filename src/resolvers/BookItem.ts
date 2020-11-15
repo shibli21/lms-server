cd2d4136382162233eb0e819c40ae71015f7ef1c
shibli21 <syedshiblimahmud@gmail.com>
2020-11-15 20:50:25 +0600

    paginated BookItems

--- src/resolvers/BookItem.ts
+++ src/resolvers/BookItem.ts
@@ -4,11 +4,22 @@ import {
   InputType,
   Int,
   Mutation,
+  ObjectType,
   Query,
   Resolver,
 } from "type-graphql";
+import { getConnection } from "typeorm";
 import { Author } from "./../entities/Author";
 import { BookItem } from "./../entities/BookItem";
+
+@ObjectType()
+class PaginatedBookItems {
+  @Field(() => [BookItem])
+  bookItems: BookItem[];
+
+  @Field()
+  hasMore: boolean;
+}
 
 @InputType()
 class BookItemInputType {
@@ -23,6 +34,24 @@ class BookItemInputType {
 
   @Field()
   edition!: string;
+}
+
+@InputType()
+class SearchBooksInput {
+  @Field({ nullable: true })
+  title?: string;
+
+  @Field({ nullable: true })
+  author?: string;
+
+  @Field({ nullable: true })
+  category?: string;
+
+  @Field(() => Int)
+  limit: number;
+
+  @Field(() => Int)
+  offset?: number;
 }
 
 @Resolver()
@@ -52,5 +81,44 @@ export class BookItemResolver {
     }).save();
 
     return bookItem;
+  }
+
+  @Query(() => PaginatedBookItems)
+  async paginatedBookItems(
+    @Arg("input") { title, author, category, limit, offset }: SearchBooksInput
+  ): Promise<PaginatedBookItems> {
+    const realLimit = Math.min(50, limit);
+    const reaLimitPlusOne = realLimit + 1;
+
+    let booksQB = getConnection()
+      .getRepository(BookItem)
+      .createQueryBuilder("b")
+      .leftJoinAndSelect("b.books", "book")
+      .leftJoinAndSelect("b.author", "author")
+      .orderBy(`b.id`, "ASC");
+
+    if (title) {
+      booksQB = booksQB.andWhere(`"b"."title" ilike :title`, {
+        title: `%${title}%`,
+      });
+    }
+    if (author) {
+      booksQB = booksQB.andWhere(`"author"."authorName" ilike :author`, {
+        author: `%${author}%`,
+      });
+    }
+
+    if (category) {
+      booksQB = booksQB.andWhere(`"b"."category" ilike :category`, {
+        category: `%${category}%`,
+      });
+    }
+
+    const bi = await booksQB.take(reaLimitPlusOne).skip(offset).getMany();
+
+    return {
+      bookItems: bi.slice(0, realLimit),
+      hasMore: bi.length === reaLimitPlusOne,
+    };
   }
 }
